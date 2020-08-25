01bf52f4f720b7b0a42e3ae86bf2e322bf538681
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-25 21:57:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function logShout(string){
 }
 function logWhisper(string){
   console.log(string.toLowerCase())
-} 
+}
 function sayHiToGrandma(string){if (string.toLowerCase() === string){
                 return ("I can't hear you!")
 }
       else if (string.toUpperCase() === string)
-      { return ("YES INDEED!")  
+      { return ("YES INDEED!")
       }
     else if (string === "I love you, Grandma."){
       return ("I love you, too.")
     break
     }
-      
+
     }
